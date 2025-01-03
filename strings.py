6fef47be1aa21097cf62db3e9016e915cfcb27a1
Manish Kc <manishkc2004@gmail.com>
2025-01-03 21:12:06 +0545

    Strings

---
+++ strings.py
@@ -0,0 +1,12 @@
+# strings in python can be 'single quoted' or "double quoted"
+
+a = 'hello world'
+print(a)
+
+# multiline string is assigned using three quotes
+b = '''this is 
+        a multiline string'''
+print(b)
+
+
+# you can slice the string using semicolon :
