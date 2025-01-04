b4c73395f47276406fa22ef826e274dd4273a987
Manish Kc <manishkc2004@gmail.com>
2025-01-04 15:33:20 +0545

    Variables in Python

---
+++ variable.py
@@ -0,0 +1,16 @@
+x = 5 #this is integer
+y = "Hello"  # this is string (you can use single or double quote)
+
+
+#If you want to specify the data type of a variable, this can be done with casting.
+
+x = str(3) # x will be '3' as it is string
+y = int(3) # y will be 3 
+z = float(3) # z will be 3.0 (gives decimal value too)
+
+
+# variable name are case-sensitive. so, 
+
+a = 5
+#this a is different from A.
+A = 10
