0cb71a7948d850f5f26257c3d8eec9a8650faa28
Manish Kc <manishkc2004@gmail.com>
2025-01-04 18:46:43 +0545

    sets in python

---
+++ sets.py
@@ -0,0 +1,17 @@
+#set is an collection of unique elements
+#set is  unordered collection
+#set can have any data types.
+#it is just like sets you studied in maths.
+
+my_set = {1, 2, 3}
+print(my_set)  #output: {1, 2, 3}
+
+# to create empty set
+empty_set = set()
+#dont use set = {} as it will create dictionary
+print (empty_set)
+
+e = {1,2,2,3,3,4}
+print(e)  #output: {1, 2, 3, 4}
+#elements are not repeated in sets
+
