df2cf136332f3bceadd7f64becb102a83b5f787f
Manish Kc <manishkc2004@gmail.com>
2025-01-04 17:20:47 +0545

    List in Python

---
+++ lists.py
@@ -0,0 +1,15 @@
+# list are container that stores a set of values of any data types.
+
+# list is different than arrays as array stores only of same data types.
+
+List = ['apple','banana', 3, 4.05, 'berry']
+
+print (List)  #output will be ['apple','banana', 3, 4.05, 'berry']
+
+print (List[0]) #output will be apple
+
+#you can also change data in list.
+
+List[0] = 'cherry'
+
+print(List[0]) #output will now be cherry.
