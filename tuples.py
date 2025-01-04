b8f1068d571d5ae322f2402d69104201829cb800
Manish Kc <manishkc2004@gmail.com>
2025-01-04 17:59:17 +0545

    tuples

---
+++ tuples.py
@@ -0,0 +1,11 @@
+#tuple is an immutable datatype in python
+
+a = () #this is an empty tuple
+a = (1,) # tuple with only one elements needs comma
+
+
+tuple = (1,2,3,4, "manish")
+print(tuple)
+print(tuple[0])
+
+tuple[0] = 'change' #this will give error as tuple is immutable
