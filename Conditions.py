dd5ab4a0e35b0a46c25f01fc43730d5dd922f55a
Manish Kc <manishkc2004@gmail.com>
2025-01-05 18:15:19 +0545

    Conditions in python

---
+++ Conditions.py
@@ -0,0 +1,21 @@
+# if condition
+#example
+a=5;
+b=10;
+if a<b:
+  print("a is smaller than b") #this will bw printed cause condition is true
+
+#in python you have to make sure of indentation otherwise it thows you an error.
+
+
+#elif is just like "if else/ else if "
+
+elif a == b:
+  print("a is equal to b") #this will not be printed cause condition is false
+  
+  
+#else. if both condition fails then this is printed
+
+else:
+  print("a is greater than b") 
+  
