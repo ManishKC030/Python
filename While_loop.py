d542ba800b38cf8da56f425ec277754fbbb6c3fd
Manish Kc <manishkc2004@gmail.com>
2025-01-05 19:00:34 +0545

    While Lopp

---
+++ While_loop.py
@@ -0,0 +1,11 @@
+#while loop repeats as long as condition is true
+
+#syntax:
+#while condition:
+   #code to be executed
+   
+i=1;
+while i<10:
+    print(i);
+# if you do this then condition will always be true and 1 will be printed forever
+    i += 1; #remember to increment     
