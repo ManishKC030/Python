6e16bf0f79c64154a586818e237f6188e59d6cb9
Manish Kc <manishkc2004@gmail.com>
2025-01-05 20:34:44 +0545

    For Loop in python

---
+++ For_Loop.py
@@ -0,0 +1,13 @@
+#for loop is used to iterate over the sequence
+
+#syntax:
+# for iems in sequence:
+     #code to be executed
+     
+fruits = ["apple", "banana", "cherry", "berry"]
+for x in fruits:
+    print(x)
+    
+
+for i in range (0, 10):
+    print (i) #output will be 0 to 9
