5fd6bcb2d6909bea87006bdd1fd35e9c17513416
Manish Kc <manishkc2004@gmail.com>
2025-01-07 18:46:28 +0545

    Functions in python

--- Functions.py
+++ Functions.py
@@ -7,4 +7,34 @@
 #creating a function
 def my_function():
     print("Hello World!")
+#this wont give any output as function is not called.
+
+#calling a function
+my_function()  # Outputs: Hello World!
+
+
+#Arguments are information that are passed into functions.
+
+def greet(name):
+    print("Hello " + name)
+    
+greet("John") #output is Hello John
+
+
+#default arguments
+def greet(name = "Guest"):
+    print("Hello " + name)
+    
+greet() #output is Hello Guest
+greet("John") #output is Hello John
+
+
+# Return Statement
+# The return statement is used to return a value from a function.
+
+def add(Num1 , Num2):
+    return Num1 + Num2
+
+result = add(2 , 3)
+print(result) #output is 5
 
