5b1819f1a435ef1f4995e15e40719576460f02e5
Manish Kc <manishkc2004@gmail.com>
2025-01-07 18:35:16 +0545

    Functions in python

---
+++ Functions.py
@@ -0,0 +1,10 @@
+#Functions is block of code which only reuns when it is called.
+#Functions are reusable code blocks.
+
+#In python function is defined using 'def' keyword.
+
+
+#creating a function
+def my_function():
+    print("Hello World!")
+
