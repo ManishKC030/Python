609d9f8d30fd0aea49bb2d0c797b8947925b21c9
Manish Kc <manishkc2004@gmail.com>
2025-01-08 18:02:28 +0545

    Recussion in Python

--- Recursion.py
+++ Recursion.py
@@ -6,3 +6,16 @@
 #  Recursive Case: The part of the function where it calls itself with a smaller or simpler argument.
 
 
+#here's simple example
+
+def recursive_sum(n):
+    # Base Case: If n is 1, return 1
+    if n == 1:
+        return 1
+    # Recursive Case: If n is greater than 1, call the function with n-1
+    else:
+        return n + recursive_sum(n-1)
+    
+#calling the function
+print(recursive_sum(5))  # Output:15 (5+4+3+2+1)
+
