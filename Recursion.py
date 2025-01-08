d44817f60dce77810aca52aa5989603964677bf6
Manish Kc <manishkc2004@gmail.com>
2025-01-08 17:55:52 +0545

    Recussion in Python

--- Recursion.py
+++ Recursion.py
@@ -1,1 +1,8 @@
 # Recursion means function calling itself
+
+# A recursive function must have:
+
+#  Base Case: The condition under which the function stops calling itself to prevent infinite recursion.
+#  Recursive Case: The part of the function where it calls itself with a smaller or simpler argument.
+
+
