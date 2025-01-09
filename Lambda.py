0d137b3cd45acc846edcd415c3d530814d89df0d
Manish Kc <manishkc2004@gmail.com>
2025-01-09 17:37:08 +0545

    Lambda in Python

--- Lambda.py
+++ Lambda.py
@@ -1,2 +1,12 @@
 # A lambda function is a small anonymous function defined using the lambda keyword.
 # It can take any number of arguments, but can only have one expression.
+# lanbda function is a small anonymous function.
+
+
+#syntax=  lambda arguments : expression
+
+add = lambda x,y : x+y
+print(add(5,6))  #output:11
+
+sub = lambda a : a - 5
+print(sub(10)) #output :5
