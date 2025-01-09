13606d9e425330315a168bbc3c0e9f4abc27f920
Manish Kc <manishkc2004@gmail.com>
2025-01-09 18:18:15 +0545

    File Handling in Python

---
+++ File Handling/fileOpen.py
@@ -0,0 +1,7 @@
+# File handling in python is a way to manipulate the files.
+# It includes reading, writing, and modifying the files.
+
+#Python provides a built in Open() function to work with files.
+# The Open() function opens and returns a file object, which takes 2 arhuments:
+# 1. The name of the file to be opened
+# 2. The mode in which the file is to be opened.
