c3cf04a03619bde0872da71eeddf2f21ce28c674
Manish Kc <manishkc2004@gmail.com>
2025-01-10 09:10:59 +0545

    File Handling in Python

--- File Handling/fileDelete.py
+++ File Handling/fileDelete.py
@@ -3,4 +3,14 @@
 
 import os
 # Delete a file
-os.remove("")
+os.remove("delete.txt") #this deleted file named delete.txt
+# To avoid getting an error, you might want to check if the file exists before you try to delete it.
+
+
+# Now you can check if file is deleted or not
+import os
+if os.path.exists('delete.txt'):
+    print("file exists")
+else:
+    print("File doesn't exists")
+# output : File doesn't exists as we deleted in above section.
