f83f34ec174a0ab0c652fa6ac841db6292b7fdfb
Manish Kc <manishkc2004@gmail.com>
2025-01-10 09:04:28 +0545

    File Handling in Python

---
+++ File Handling/fileDelete.py
@@ -0,0 +1,6 @@
+# To delete a file, you must import the OS module, and run its os.remove() function.
+# The os.remove() function deletes the specified file.
+
+import os
+# Delete a file
+os.remove("")
