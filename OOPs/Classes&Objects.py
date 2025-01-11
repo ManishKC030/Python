5fe8e865846822a8ac5d3fed68a997ffb5c465c1
Manish Kc <manishkc2004@gmail.com>
2025-01-11 20:10:05 +0545

    Classes & Objects in Python

--- OOPs/Classes&Objects.py
+++ OOPs/Classes&Objects.py
@@ -8,4 +8,17 @@
 #   - A blueprint or a template for creating objects is called class.
 #Class is always written in PascalCase.
 
+#To create a 'class', use the keyword "class".
+
+class MyClass :
+    x=5   #we created class MyClass with Property named X.
+
+#Now we can use class to create objects.
+#Object is an instance of a class.
+#Object is created using the class name.
+#Object is always written in camelCase.
+
+obj = MyClass()  #we created object obj from class MyClass.
+print(obj.x)  #we printed the value of property X of object obj.
+
 
