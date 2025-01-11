1a5a55d9dbad145e2b2b6c91ca3929a331c6b525
Manish Kc <manishkc2004@gmail.com>
2025-01-11 19:02:55 +0545

    Classes & Objects in Python

--- OOPs/Classes&Objects.py
+++ OOPs/Classes&Objects.py
@@ -1,2 +1,11 @@
 # OOP stands fot Object Oriented Programming.
 # It is a programming paradigm that uses objects and classes to design applications and programs.
+
+
+# Classes and Objects
+
+# Class:
+#   - A blueprint or a template for creating objects is called class.
+#Class is always written in PascalCase.
+
+
