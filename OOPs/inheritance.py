c543ef09ea11c8f3c68963154e250c7b265b3b22
Manish Kc <manishkc2004@gmail.com>
2025-01-12 13:14:27 +0545

    Inheritance in Python

--- OOPs/inheritance.py
+++ OOPs/inheritance.py
@@ -4,3 +4,14 @@
 # Single Inheritance: A child class inherits from one parent class.
 # Multiple Inheritance: A child class inherits from multiple parent classes.
 # Multilevel Inheritance: A class inherits from a derived class.
+
+class Animal:   #Parent Class
+    def sound(self):
+      return "I am an Animal"
+        
+class Dog(Animal):  #CHild Class
+    def sound(self):
+       return "Dog Barks. Woof!!"
+        
+dog = Dog()
+print(dog.sound())
