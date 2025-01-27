7eb31bd30961326328922eb7809e388e3869c7a4
Manish K.C. <159449738+ManishKC030@users.noreply.github.com>
2025-01-27 17:53:29 +0545

    Update user_input.py

--- user_input.py
+++ user_input.py
@@ -1,5 +1,5 @@
 # In Python, you can get input from the user using the built-in input() function.
-# The input() function returns a string, so you need to convert it to datatype as your wish
+# The input() function returns a string, so you need to convert it to datatype as you wish
 # For example, if you want to get an integer from the user, you can use int()
 
 a = input("Enter your message:")
