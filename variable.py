b0a0689120bb965e339b03481a23382f8361cf2d
Manish K.C. <159449738+ManishKC030@users.noreply.github.com>
2025-01-27 17:53:06 +0545

    variable.py

--- variable.py
+++ variable.py
@@ -1,15 +1,15 @@
 x = 5 #this is integer
-y = "Hello"  # this is string (you can use single or double quote)
+y = "Hello"  # this is a string (you can use single or double quote)
 
 
 #If you want to specify the data type of a variable, this can be done with casting.
 
-x = str(3) # x will be '3' as it is string
+x = str(3) # x will be '3' as it is a string
 y = int(3) # y will be 3 
 z = float(3) # z will be 3.0 (gives decimal value too)
 
 
-# variable name are case-sensitive. so, 
+# variable names are case-sensitive. so, 
 
 a = 5
 #this a is different from A.
